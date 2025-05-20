60d96c9cbefa3154f3577bd31df7f5327caca4c4
sany <dx2009118197@163.com>
2025-05-20 23:35:02 +0800

    update scale to "auto" for DP-1

--- hosts/sany-nixos/home.nix
+++ hosts/sany-nixos/home.nix
@@ -6,9 +6,9 @@
         # Configure your Display resolution, offset, scale and Monitors here, use `hyprctl monitors` to get the info.
         #   highres:      get the best possible resolution
         #   auto:         position automatically
-        #   1.5:          scale to 1.6 times
+        #   auto:         auto scale 
         #   bitdepth,10:  enable 10 bit support
-        monitor = "DP-1,highres,auto,1.6,bitdepth,10";
+        monitor = "DP-1,highres,auto,auto,bitdepth,10";
       };
     };
   };
